fef50389edf6e70c805246ed5d7ed12fc4e9b397
JordiCorbilla <jordi.coll.corbilla@gmail.com>
2016-08-21 10:18:43 +0100

    Adding initial version of the site with knockoutjs and bindings.

---
+++ static/Scripts/connectivity.js
@@ -0,0 +1,55 @@
+function ping(ip, callback) {
+
+    if (!this.inUse) {
+        this.status = 'unchecked';
+        this.inUse = true;
+        this.callback = callback;
+        this.ip = ip;
+        var _that = this;
+        this.img = new Image();
+        this.img.onload = function () {
+            _that.inUse = false;
+            _that.callback('ONLINE');
+
+        };
+        this.img.onerror = function (e) {
+            if (_that.inUse) {
+                _that.inUse = false;
+                _that.callback('ONLINE', e);
+            }
+
+        };
+        this.start = new Date().getTime();
+        this.img.src = "http://" + ip;
+        this.timer = setTimeout(function () {
+            if (_that.inUse) {
+                _that.inUse = false;
+                _that.callback('timeout');
+            }
+        }, 1500);
+    }
+}
+var PingModel = function (servers) {
+    var self = this;
+    var myServers = [];
+    ko.utils.arrayForEach(servers, function (location) {
+        myServers.push({
+            name: location,
+            status: ko.observable('PENDING')
+        });
+    });
+    self.servers = ko.observableArray(myServers);
+    ko.utils.arrayForEach(self.servers(), function (s) {
+        s.status('CONTACTING');
+        new ping(s.name, function (status, e) {
+            s.status(status);
+        });
+    });
+};
+var komodel = new PingModel([
+    'piController',
+    'pi01',
+    'pi02',
+    'pi03'
+    ]);
+ko.applyBindings(komodel);
